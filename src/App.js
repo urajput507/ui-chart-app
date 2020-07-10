63212401b507c404776d9b8b45d695025b1f6380
Usman Rajput <66204853+urajput507@users.noreply.github.com>
2020-07-10 13:52:19 -0700

    M Ui and Chart js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Paper from './paper';
-
+import Piechart from './piechart';
 function App() {  
   return (
   <div>
   <Paper />
+  <Piechart />
   </div>
   
   );
