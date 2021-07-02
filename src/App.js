2761ee7c57457e8a4f8ea71c8c88fe308524c3e1
Usman Rajput <66204853+urajput507@users.noreply.github.com>
2021-07-03 03:16:15 +0500

    updtae

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// import React, { Component } from 'react';
 import './App.css';
 import Axios from 'axios';
 class App extends Component{
