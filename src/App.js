b2c847749e1354576ae63a35c2cce9511132ebad
Usman Rajput <66204853+urajput507@users.noreply.github.com>
2021-07-03 02:50:44 +0500

    final year project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import React from 'react';
-// import Paper from './paper';
-// import Piechart from './piechart';
-import Menu from './menu';
-// import Divider from './divider';
-import Table from './table';
-// import Grid from './grid';
-
-function App() {
-  return (
-    <div>
-      <Menu />      
-      <Table />
-      {/* <Divider/> */}
-    </div>
-
-  );
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Axios from 'axios';
+class App extends Component{
+  state ={
+    text : ""
+  };
+  handleAdd= async e =>{
+        await this.setState({
+      text: e.target.value
+    })
+  }
+  handleSubmit = e =>{
+    e.preventDefault();
+    console.log(this.state.text);
+    let formData = new FormData();
+    formData.append("text",this.state.text);
+    const url = "//localhost/react-backend/";
+    Axios.post(url,formData)
+    .than(res=> console.log(res.data))
+    .catch(err=> console.log(err))
+  }
+render()
+{
+return(
+<div className="App-header">
+<br/>
+<button
+    type="button" className="btn btn-success"
+    onClick={(e) => {
+      e.preventDefault();
+      window.location.href='http://localhost/fyppro/search.php';
+      }}
+> Search Here</button>
+ <button
+    type="button" className="btn btn-success"
+    onClick={(e) => {
+      e.preventDefault();
+      window.location.href='http://localhost/fyppro/admin_login.php';
+      }}
+> Admin Login</button>
+</div>
+);
+} 
 }
-
 export default App;
